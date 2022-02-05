4a0b6bd4d603407dbb16d2b67017993024a9c4b8
Basil Benedict Victor <basilbennedict@gmail.com>
2022-02-05 10:59:01 +0530

    Session on common widgets

--- app/src/main/java/com/ascii/asciiappdevsession/MainActivity.java
+++ app/src/main/java/com/ascii/asciiappdevsession/MainActivity.java
@@ -2,69 +2,75 @@ package com.ascii.asciiappdevsession;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView level_1;
+    TextView nameTxt;
+    Button submitBtn;
+    TextInputEditText nameTet;
+    SwitchMaterial darkSwitch;
+    FloatingActionButton listFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear_layout_v_demo);
+        setContentView(R.layout.activity_main);
 
-        level_1 = (TextView) findViewById(R.id.level_1);
+//        nameTxt = (TextView) findViewById(R.id.name_txt);
+//        nameTet = (TextInputEditText) findViewById(R.id.name_tet);
+//        submitBtn = (Button) findViewById(R.id.submit_btn);
+//        darkSwitch = (SwitchMaterial) findViewById(R.id.dark_switch);
+//        listFab = (FloatingActionButton) findViewById(R.id.list_fab);
 
-        Snackbar snackbar = Snackbar.make(level_1, "Hello", Snackbar.LENGTH_INDEFINITE); // shows a snackbar indefinitely
+//        submitBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                nameTxt.setText(nameTet.getText());
+//                nameTet.setText(null);
+//                nameTet.clearFocus();
+//            }
+//        });
 
-        snackbar.setAction("DISMISS ME", new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // defines what happens when action button in snackbar is clicked
-                level_1.setText("Snackbar dismissed"); //changing the text of TextView on action click
-                snackbar.dismiss();
-            }
-        });
+//        darkSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+//                if(isChecked) {
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                }
+//                else
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//            }
+//        });
 
-        snackbar.show(); // finally show the snackbar
-    }
+//        listFab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "To be implemented", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
-    @Override
-    public void onBackPressed() {
-        AlertDialog dialog = new MaterialAlertDialogBuilder(MainActivity.this)
-                .setTitle("EXIT?") //sets title of the dialog
-                .setMessage("Are you sure you want to leave the app?") // sets message of the dialog
-                .setCancelable(false) // this makes dialog non dismissible when clicked outside
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        // defines what happens when positive button clicked
-                        MainActivity.super.onBackPressed(); // we are calling the onBackPressed method originally defined in AppCompatActivity
-                        // by calling the super/parent method we are performing what normally would happen when back button was clicked
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        // defines what happens when negative button clicked
-                        // cancel, dialog dismissed
-                    }
-                })
-                .setNeutralButton("Show Toast", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        // defines what happens when neutral button clicked
-                        Toast.makeText(MainActivity.this, "Neutral Button Clicked", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .show();
+//        nameTxt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "TextViews are also clickable", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
     }
 }
